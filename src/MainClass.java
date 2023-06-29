a5e7e058d16e422709aa153d7b09a40e149b4c59
KostiaMoroz7 <morozkostia7@gmail.com>
2023-06-29 17:09:17 +0300

    Next

--- src/MainClass.java
+++ src/MainClass.java
@@ -14,14 +14,14 @@ public class MainClass {
                 }
                 if (robotMassive.get(i).getMainString().contains(enteredLetter) & (enteredLetter.length() == 1)) {
                     Robot.checkDamage(robotMassive.get(i), enteredLetter);
-                    if (robotMassive.get(i).getHealthPoint() == 0) {
+                    if (robotMassive.get(i).getHealthPoint() <= 0) {
                         menuRobots.removeAndPrintDeadRobotInfo(robotMassive);
                         i--;
                     }
                     continue;
                 } else {
                     Robot.checkDamage(robotMassive.get(i), Robot.checkWrongEnters(robotMassive.get(i).getMainString(), enteredLetter, robotMassive));
-                    if (robotMassive.get(i).getHealthPoint() == 0) {
+                    if (robotMassive.get(i).getHealthPoint() <= 0) {
                         menuRobots.removeAndPrintDeadRobotInfo(robotMassive);
                         i--;
                     }

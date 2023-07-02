e8c7f833a214ff9df488fb63ae30da60f9c7b3fb
KostiaMoroz7 <morozkostia7@gmail.com>
2023-07-02 14:02:44 +0300

    Upgrage_With_Defence_Letters

--- src/CreateRobot.java
+++ src/CreateRobot.java
@@ -6,11 +6,17 @@ public class CreateRobot {
     private String robotName;
     private int healthPoint = 100;
     private String mainString = "qweasdzxc";
+
+    private String defence = "tygh";
     private StringBuffer damageString;
     private String damageLetters;
-
     private String enteredLetterWithDamageRobot = " ";
 
+    private StringBuffer defenceString;
+
+    private String defenceLetters;
+
+    private String enteredDefenceLetter = " ";
 
     public CreateRobot(String externalRobotName) {
         robotName = externalRobotName;
@@ -18,8 +24,14 @@ public class CreateRobot {
         for (int i = 0; i < 4; i++) {
             int randomIndex = (int) (Math.random() * (9 - i));
             damageString.delete(randomIndex, randomIndex + 1);
-            String enteredLetterWithDamageRobot = " ";
         }
+        String enteredLetterWithDamageRobot = " ";
+        defenceString = new StringBuffer(defence);
+        for (int i = 0; i < 2; i++) {
+            int randomIndex = (int) (Math.random() * (4 - i));
+            defenceString.delete(randomIndex, randomIndex + 1);
+        }
+        String enteredDefenceLetter = " ";
     }
 
     public String getRobotName() {
@@ -43,6 +55,14 @@ public class CreateRobot {
         this.damageLetters = damageLetters;
     }
 
+    public String getDefenceLetters() {
+        return defenceString.toString();
+    }
+
+    public void setDefenceLetters(String defenceLetters) {
+        this.defenceLetters = defenceLetters;
+    }
+
     public String getMainString() {
         return mainString;
     }
@@ -55,15 +75,47 @@ public class CreateRobot {
         this.enteredLetterWithDamageRobot = enteredLetterWithDamageRobot;
     }
 
+    public String getEnteredDefenceLetter() {
+        return enteredDefenceLetter;
+    }
+
+    public void setEnteredDefenceLetter(String enteredDefenceLetter) {
+        this.enteredDefenceLetter = enteredDefenceLetter;
+    }
+
+    private static void increaseHealth(CreateRobot damagedRobot, String compareLetter) {
+        damagedRobot.setHealthPoint(damagedRobot.getHealthPoint() - 20);
+        damagedRobot.setEnteredLetterWithDamageRobot(damagedRobot.getEnteredLetterWithDamageRobot() + compareLetter);
+        System.out.println("Right on target! Good job. The health of " + "\"" + damagedRobot.getRobotName() + "\"" + " Robot" + " decreased for - 20 points");
+    }
+
     public static int checkDamage(CreateRobot damagedRobot, String compareLetter) {
+        String enter;
         if (damagedRobot.getEnteredLetterWithDamageRobot().contains(compareLetter.toLowerCase())) {
             System.out.println("Key is not active");
             return damagedRobot.getHealthPoint();
         }
         if (damagedRobot.getDamageLetters().contains(compareLetter.toLowerCase())) {
-            damagedRobot.setHealthPoint(damagedRobot.getHealthPoint() - 20);
-            damagedRobot.setEnteredLetterWithDamageRobot(damagedRobot.getEnteredLetterWithDamageRobot() + compareLetter);
-            System.out.println("Right on target! Good job. The health of " + "\"" + damagedRobot.getRobotName() + "\"" + " Robot" + " decreased for - 20 points");
+            if (damagedRobot.getEnteredDefenceLetter().length() > 2){
+                System.out.println("There are no more keys for protection");
+                CreateRobot.increaseHealth(damagedRobot, compareLetter);
+                return damagedRobot.getHealthPoint();
+            }
+            System.out.println("Select letter to defence from TYGH.");
+            enter = MenuRobots.getValueFromConsoleString();
+            if (damagedRobot.getEnteredDefenceLetter().contains(enter)) {
+                System.out.println("Protection key isn't active");
+                CreateRobot.increaseHealth(damagedRobot, compareLetter);
+                return damagedRobot.getHealthPoint();
+            }
+            if (damagedRobot.getDefenceLetters().contains(enter.toLowerCase())) {
+                System.out.println("The damage did not pass, the protection is activated");
+                damagedRobot.setEnteredDefenceLetter(damagedRobot.getEnteredDefenceLetter() + enter);
+                return damagedRobot.getHealthPoint();
+            } else {
+                System.out.println("Protection isn't activated");
+            }
+            CreateRobot.increaseHealth(damagedRobot, compareLetter);
             return damagedRobot.getHealthPoint();
         } else {
             System.out.println("Key does no damage");
--- src/MainClass.java
+++ src/MainClass.java
@@ -8,6 +8,7 @@ public class MainClass {
         do {
             for (int i = 0; i < robotMassive.size(); i++) {
                 MenuRobots.printRobotsMenu(robotMassive.get(i).getRobotName());
+                System.out.println("Selected defence letter (out for testing): " + robotMassive.get(i).getDefenceLetters());
                 enteredLetter = MenuRobots.getValueFromConsoleString();
                 if (Robot.isEnd(enteredLetter, robotMassive) == true) {
                     System.exit(0);
@@ -17,6 +18,9 @@ public class MainClass {
                     if (robotMassive.get(i).getHealthPoint() <= 0) {
                         menuRobots.removeAndPrintDeadRobotInfo(robotMassive);
                         i--;
+                        if (robotMassive.size() < 2){
+                            break;
+                        }
                     }
                     continue;
                 } else {
@@ -24,6 +28,9 @@ public class MainClass {
                     if (robotMassive.get(i).getHealthPoint() <= 0) {
                         menuRobots.removeAndPrintDeadRobotInfo(robotMassive);
                         i--;
+                        if (robotMassive.size() < 2){
+                            break;
+                        }
                     }
                     continue;
                 }
